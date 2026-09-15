e015d2da4979fa7794ac31922801d644a8d7325e
Ashwin Dhakal <ashwindhakal97@gmail.com>
2026-09-15 11:19:19 -0400

    adding class flutter projects

---
+++ 4_learning_concept_of_stateless_widget_scaffold_and_hot_reload/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp (MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: Container(
+            color: Colors.red,
+            child: Text('Stateless'),
+          ),
+        ),
+      ),
+    );
+  }
+}
